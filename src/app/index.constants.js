71054d4053d87aa10258ae0e0307d3eecc02dfe0
Pandiyan <pmurugan@burning-glass.com>
2016-11-16 19:23:12 +0530

    Initial commit
    Signed-off-by: Pandiyan

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('pandiyanMe')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('pandiyanMe', [
+      'ngAnimate'
+      , 'ngCookies'
+      , 'ngTouch'
+      , 'ngSanitize'
+      , 'ngMessages'
+      , 'ngAria'
+      , 'ngResource'
+      , 'ui.router'
+      , 'ngMaterial'
+      , 'toastr'
+    ]);
+
+})();
