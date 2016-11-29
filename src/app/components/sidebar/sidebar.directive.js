af434b4b48af5149c7527c9f44fd726fd2294907
Pandiyan <pandiyanit7@gmail.com>
2016-11-29 08:42:46 +0530

    Added sidebar items

--- src/app/components/sidebar/sidebar.directive.js
+++ src/app/components/sidebar/sidebar.directive.js
@@ -26,7 +26,25 @@
         }
     }
     /* @ngInject */
-    function SidebarController() {
+    function SidebarController($mdDialog) {
         var vm = this;
+        vm.sidebarMenu = [
+            { name: 'About', value: 'About' },
+            { name: 'Blog', value: 'Blog' },
+            { name: 'Resume', value: 'Resume' },
+            { name: 'Projects', value: 'Projects' }
+        ];
+
+        vm.navigateTo = function (to, event) {
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .title('Navigating')
+                    .textContent('Imagine being taken to ' + to)
+                    .ariaLabel('Navigation demo')
+                    .ok('Neat!')
+                    .targetEvent(event)
+            );
+        };
+
     }
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,13 +6,30 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($timeout, webDevTec, toastr, $mdDialog) {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1479296275456;
     vm.showToastr = showToastr;
+    vm.sidebarMenu = [
+            { name: 'About', value: 'About' },
+            { name: 'Blog', value: 'Blog' },
+            { name: 'Resume', value: 'Resume' },
+            { name: 'Projects', value: 'Projects' }
+        ];
+
+        vm.navigateTo = function (to, event) {
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .title('Navigating')
+                    .textContent('Imagine being taken to ' + to)
+                    .ariaLabel('Navigation demo')
+                    .ok('Neat!')
+                    .targetEvent(event)
+            );
+        };
 
     activate();
 
